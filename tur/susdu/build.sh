c4d26cf178c5c5424b8e0c28407ea40f0ade418d
Inrryoff <inrrymacollemcastroreis@gmail.com>
2026-04-19 19:28:06 -0300

    Create build.sh

---
+++ tur/susdu/build.sh
@@ -0,0 +1,17 @@
+TERMUX_PKG_HOMEPAGE=https://github.com
+TERMUX_PKG_DESCRIPTION="Uma alternativa moderna ao tsu para diferentes root managers"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@inrryoff"
+TERMUX_PKG_VERSION=1.0.0
+TERMUX_PKG_SKIP_SRC_EXTRACT=true
+TERMUX_PKG_PLATFORM_INDEPENDENT=true
+
+termux_step_get_source() {
+	termux_download "https://githubusercontent.com" \
+		"$TERMUX_PKG_BUILDER_DIR/susdu" \
+		709b76060b85fc68f211c1c11380a253ac3b95905fcdc5215cef29a4777bf2ee
+}
+
+termux_step_make_install() {
+	install -Dm755 "$TERMUX_PKG_BUILDER_DIR/susdu" "$TERMUX_PREFIX/bin/susdu"
+}
